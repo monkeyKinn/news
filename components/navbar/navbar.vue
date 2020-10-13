a16595971a2831e27d943dd9ebbc6035286fc9cf
jinsc <jinshengcong@163.com>
2020-10-13 15:53:23 +0800

    基础的自定义导航栏

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <view class="navbar">
+    <view class="navbar-fixed">
+      <!-- 搜索 -->
+      <view class="navbar-search">
+        <view class="navbar-search-icon"></view>
+        <view class="navbar-search-text">app,vue</view>
+      </view>
+    </view>
+    <view style="height: 45px;"></view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .navbar {
+    .navbar-fixed {
+      // 两行变一行 flex布局
+      display: flex;
+      // 水平垂直居中显示
+      justify-content: center;
+      align-items: center;
+      padding: 0 15px;
+
+      width: 100%;
+      height: 45px;
+      background-color: $mk-base-color;
+      // 让里面的东西在盒子内显示
+      box-sizing: border-box;
+      // 固定定位
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 99;
+
+      .navbar-search {
+        // 两行变一行 flex布局
+        display: flex;
+        // 垂直居中显示
+        align-items: center;
+        padding: 0 10px;
+        width: 100%;
+        height: 30px;
+        // 圆角
+        border-radius: 30px;
+        background-color: #fff;
+
+        .navbar-search-icon {
+          height: 10px;
+          width: 10px;
+          border: 1px red solid;
+          margin-right: 10px;
+        }
+
+        .navbar-search-text {
+          font-size: 12px;
+          color: #999999;
+        }
+      }
+    }
+
+  }
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,9 +1,10 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+    <!-- 自定义导航栏 -->
+    <navbar></navbar>
+    <view v-for="item in 100">
+      {{item}} 内容
+    </view>
 	</view>
 </template>
 
@@ -23,30 +24,6 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss" scoped>
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
